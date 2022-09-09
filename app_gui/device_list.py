75641427a6cf0e3307c247358d11f4b62f7d7c22
ChangeinX <userdata0@gmail.com>
2022-09-09 18:40:46 -0500

    Made the GUI for device_list.py to be triggered by button in main.py The window that opens displays two tables, a dynamic search bar, and buttons for each table.

---
+++ app_gui/device_list.py
@@ -0,0 +1,194 @@
+# Device window has two tables, one for routers and one for switches, and a dynamic search bar above the tables.
+
+
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QTableWidget, QTableWidgetItem, QVBoxLayout, QAbstractItemView, \
+    QHeaderView, QLineEdit, QHBoxLayout, QLabel, QPushButton
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QBrush, QColor
+
+class DeviceList(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.title = 'Device List'
+        self.left = 10
+        self.top = 10
+        self.width = 640
+        self.height = 480
+        self.initUI()
+
+    def initUI(self):
+        self.setWindowTitle(self.title)
+        self.setGeometry(self.left, self.top, self.width, self.height)
+
+        self.table_widget = QTableWidget()
+        # row count is 0 because we will add rows dynamically
+        self.table_widget.setRowCount(0)
+        self.table_widget.setColumnCount(3)
+        self.table_widget.setHorizontalHeaderLabels(['Device Name', 'Location', 'IP Address'])
+        # make header tables text black
+        for i in range(self.table_widget.columnCount()):
+            self.table_widget.horizontalHeaderItem(i).setForeground(QBrush(QColor(0, 0, 0)))
+        self.table_widget.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
+        self.table_widget.verticalHeader().setSectionResizeMode(QHeaderView.Stretch)
+        self.table_widget.setEditTriggers(QAbstractItemView.NoEditTriggers)
+        self.table_widget.setSelectionBehavior(QAbstractItemView.SelectRows)
+        self.table_widget.setSelectionMode(QAbstractItemView.SingleSelection)
+        self.table_widget.itemClicked.connect(self.on_click)
+
+        # column 0 is the device name with header 'Device Name'
+        # column 1 is the device location with header 'Location'
+        # column 2 is the device IP address with header 'IP Address'
+        self.table_widget.setItem(0, 0, QTableWidgetItem('R1'))
+        self.table_widget.setItem(0, 1, QTableWidgetItem('x.x.x.x'))
+        self.table_widget.setItem(1, 0, QTableWidgetItem('R2'))
+
+        self.table_widget.setItem(2, 0, QTableWidgetItem('S1'))
+        self.table_widget.setItem(2, 1, QTableWidgetItem('x.x.x.x'))
+        self.table_widget.setItem(3, 0, QTableWidgetItem('S2'))
+
+        # add dynamic search bar above the table
+        self.search_bar = QLineEdit()
+        self.search_bar.setPlaceholderText('Search')
+        self.search_bar.textChanged.connect(self.search)
+
+        # add label above the search bar
+        self.search_label = QLabel('Search:')
+
+        # add label above the table
+        self.table_label = QLabel('Devices:')
+
+        # add table for switches on the right side of the window
+        self.table_widget2 = QTableWidget()
+        # row count is 0 because we will add rows dynamically
+        self.table_widget2.setRowCount(0)
+        self.table_widget2.setColumnCount(3)
+        self.table_widget2.setHorizontalHeaderLabels(['Device Name', 'Location', 'IP Address'])
+        # make the horizontal header labels text black instead of white
+        for i in range(self.table_widget2.columnCount()):
+            self.table_widget2.horizontalHeaderItem(i).setForeground(QBrush(QColor(0, 0, 0)))
+
+        self.table_widget2.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
+        self.table_widget2.verticalHeader().setSectionResizeMode(QHeaderView.Stretch)
+        self.table_widget2.setEditTriggers(QAbstractItemView.NoEditTriggers)
+        self.table_widget2.setSelectionBehavior(QAbstractItemView.SelectRows)
+        self.table_widget2.setSelectionMode(QAbstractItemView.SingleSelection)
+        self.table_widget2.itemClicked.connect(self.on_click)
+
+        # four buttons for table 1
+        # button 1 is 'Add'
+        # button 2 is 'Edit'
+        # button 3 is 'Delete'
+        # button 4 is 'Download Config'
+        self.button1 = QPushButton('Add')
+        self.button2 = QPushButton('Edit')
+        self.button3 = QPushButton('Delete')
+        self.button4 = QPushButton('Download Config')
+
+        # add label above the buttons
+        self.button_label = QLabel('Actions:')
+
+        # place buttons below table 1
+        self.button_layout = QHBoxLayout()
+        self.button_layout.addWidget(self.button1)
+        self.button_layout.addWidget(self.button2)
+        self.button_layout.addWidget(self.button3)
+        self.button_layout.addWidget(self.button4)
+
+        # four buttons for table 2
+        self.add_button2 = QPushButton('Add')
+        self.edit_button2 = QPushButton('Edit')
+        self.delete_button2 = QPushButton('Delete')
+        self.download_button2 = QPushButton('Download Config')
+
+        # add label above the buttons
+        self.button_label2 = QLabel('Actions:')
+
+        # place buttons below table 2
+        self.button_layout2 = QHBoxLayout()
+        self.button_layout2.addWidget(self.add_button2)
+        self.button_layout2.addWidget(self.edit_button2)
+        self.button_layout2.addWidget(self.delete_button2)
+        self.button_layout2.addWidget(self.download_button2)
+
+        # place table 1 and table 2 side by side
+        self.table_layout = QHBoxLayout()
+        self.table_layout.addWidget(self.table_widget)
+        self.table_layout.addWidget(self.table_widget2)
+
+        # place search bar above table 2
+        self.search_layout2 = QHBoxLayout()
+
+        # place search bar above table 1
+        self.search_layout = QHBoxLayout()
+        self.search_layout.addWidget(self.search_label)
+        self.search_layout.addWidget(self.search_bar)
+
+        # add the search bar and table to the layout
+        self.layout = QVBoxLayout()
+        self.layout.addWidget(self.search_label)
+        self.layout.addWidget(self.search_bar)
+        self.layout.addWidget(self.table_label)
+        self.layout.addWidget(self.table_widget)
+        self.layout.addWidget(self.button_label)
+        self.layout.addLayout(self.button_layout)
+        self.layout.addWidget(self.table_widget2)
+        self.layout.addWidget(self.button_label2)
+        self.layout.addLayout(self.button_layout2)
+        self.setLayout(self.layout)
+
+        # style the window for a nice blue look
+        self.setStyleSheet("background-color: #2c3e50; color: #ecf0f1;")
+        self.table_widget.setStyleSheet("background-color: #34495e; color: #ecf0f1;")
+        self.table_widget2.setStyleSheet("background-color: #34495e; color: #ecf0f1;")
+        self.search_bar.setStyleSheet("background-color: #34495e; color: #ecf0f1;")
+        self.search_label.setStyleSheet("color: #ecf0f1;")
+        self.table_label.setStyleSheet("color: #ecf0f1;")
+        self.button_label.setStyleSheet("color: #ecf0f1;")
+        self.button_label2.setStyleSheet("color: #ecf0f1;")
+        self.button1.setStyleSheet("background-color: #34495e; color: #ecf0f1;")
+        self.button2.setStyleSheet("background-color: #34495e; color: #ecf0f1;")
+        self.button3.setStyleSheet("background-color: #34495e; color: #ecf0f1;")
+        self.button4.setStyleSheet("background-color: #34495e; color: #ecf0f1;")
+        self.add_button2.setStyleSheet("background-color: #34495e; color: #ecf0f1;")
+        self.edit_button2.setStyleSheet("background-color: #34495e; color: #ecf0f1;")
+        self.delete_button2.setStyleSheet("background-color: #34495e; color: #ecf0f1;")
+        self.download_button2.setStyleSheet("background-color: #34495e; color: #ecf0f1;")
+        # self.button1.clicked.connect(self.add_row)
+        # self.button2.clicked.connect(self.edit_row)
+        # self.button3.clicked.connect(self.delete_row)
+        # self.button4.clicked.connect(self.download_config)
+        # self.add_button2.clicked.connect(self.add_row2)
+        # self.edit_button2.clicked.connect(self.edit_row2)
+        # self.delete_button2.clicked.connect(self.delete_row2)
+        # self.download_button2.clicked.connect(self.download_config2)
+
+
+        self.show()
+
+    def search(self):
+        # search the table for the text in the search bar
+        # if the text is found, highlight the row
+        # if the text is not found, remove the highlight
+        search_text = self.search_bar.text()
+        for row in range(self.table_widget.rowCount()):
+            for column in range(self.table_widget.columnCount()):
+                item = self.table_widget.item(row, column)
+                if item is not None:
+                    if search_text.lower() in item.text().lower():
+                        self.table_widget.selectRow(row)
+                        return
+        self.table_widget.clearSelection()
+
+    def on_click(self):
+        # when a row is clicked, print the text in the first column
+        for currentQTableWidgetItem in self.table_widget.selectedItems():
+            print(currentQTableWidgetItem.text())
+
+
+
+
+
+
+
+
--- app_gui/main.py
+++ app_gui/main.py
@@ -4,248 +4,57 @@
 # the second button should open a new window from another class
 # the third button should open a new window from another class
 
-# import the necessary modules
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QToolTip, QMessageBox
-from PyQt5.QtGui import QIcon, QFont
-from PyQt5.QtCore import QCoreApplication
 
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton
 
-# create a class for the GUI
+from app_gui import device_list, configure_device_window, window3
+
 class App(QWidget):
-
-    # create a method to initialize the GUI
     def __init__(self):
-        # call the init method from the parent class
         super().__init__()
-        # call the init method from the GUI class
+        self.title = 'PyQt5 App'
+        self.left = 10
+        self.top = 10
+        self.width = 640
+        self.height = 480
         self.initUI()
 
-    # create a method to initialize the GUI
     def initUI(self):
-        # set the size of the window
-        self.resize(300, 200)
-        # set the position of the window
-        self.move(300, 300)
-        # set the title of the window
-        self.setWindowTitle('App')
-        # set the icon of the window
-        self.setWindowIcon(QIcon('web.png'))
+        self.setWindowTitle(self.title)
+        self.setGeometry(self.left, self.top, self.width, self.height)
 
-        # create a button to open a new window
-        btn = QPushButton('Open Window', self)
-        # set the position of the button
-        btn.move(20, 20)
-        # set the size of the button
-        btn.resize(btn.sizeHint())
-        # connect the button to the open_window method
-        btn.clicked.connect(self.device_information)
+        self.button1 = QPushButton('Open Window 1', self)
+        self.button1.move(20, 20)
+        self.button1.clicked.connect(self.on_click1)
 
-        # create a button to open a new window
-        btn = QPushButton('Open Window', self)
-        # set the position of the button
-        btn.move(20, 60)
-        # set the size of the button
-        btn.resize(btn.sizeHint())
-        # connect the button to the open_window method
-        btn.clicked.connect(self.device_information)
+        self.button2 = QPushButton('Open Window 2', self)
+        self.button2.move(20, 60)
+        self.button2.clicked.connect(self.on_click2)
 
-        # create a button to open a new window
-        btn = QPushButton('Open Window', self)
-        # set the position of the button
-        btn.move(20, 100)
-        # set the size of the button
-        btn.resize(btn.sizeHint())
-        # connect the button to the open_window method
-        btn.clicked.connect(self.device_information)
+        self.button3 = QPushButton('Open Window 3', self)
+        self.button3.move(20, 100)
+        self.button3.clicked.connect(self.on_click3)
 
-        # create a button to exit the app
-        btn = QPushButton('Exit', self)
-        # set the position of the button
-        btn.move(20, 140)
-        # set the size of the button
-        btn.resize(btn.sizeHint())
-        # connect the button to the exit method
-        btn.clicked.connect(QCoreApplication.instance().quit)
-
-        # show the window
         self.show()
 
-    # create a method to open the window for device information
-    def device_information(self):
-        # create a new window
-        device_info = DeviceInfo()
-        # show the new window
-        device_info.show()
+    def on_click1(self):
+        self.w = device_list.DeviceList()
+        self.w.show()
 
-    # create a method to open the window for configuring devices
-    def configure_devices(self):
-        # create a new window
-        configure_devices = ConfigureDevices()
-        # show the new window
-        configure_devices.show()
+    def on_click2(self):
+        self.w = window2.Window2()
+        self.w.show()
 
-    # create a method to open the window for downloading configs
-    def download_configs(self):
-        # create a new window
-        download_configs = DownloadConfigs()
-        # show the new window
-        download_configs.show()
+    def on_click3(self):
+        self.w = window3.Window3()
+        self.w.show()
 
-    # create a method to configure Orion NPM
-    def configure_orion_npm(self):
-        # create a new window
-        oriong_view = OrionView()
-        # show the new window
-        oriong_view.show()
-
-
-# create a class for the new window
-class DeviceInfo(QWidget):
-
-    # create a method to initialize the new window
-    def __init__(self):
-        # call the init method from the parent class
-        super().__init__()
-        # call the init method from the new window class
-        self.initUI()
-
-    # create a method to initialize the new window
-    def initUI(self):
-        # set the size of the window
-        self.resize(300, 200)
-        # set the position of the window
-        self.move(600, 300)
-        # set the title of the window
-        self.setWindowTitle('Window')
-        # set the icon of the window
-        self.setWindowIcon(QIcon('web.png'))
-
-        # create a button to close the window
-        btn = QPushButton('Close', self)
-        # set the position of the button
-        btn.move(20, 20)
-        # set the size of the button
-        btn.resize(btn.sizeHint())
-        # connect the button to the close method
-        btn.clicked.connect(self.close)
-
-        # show the window
-        self.show()
-
-
-# create a class for the new window
-class ConfigureDevices(QWidget):
-
-    # create a method to initialize the new window
-    def __init__(self):
-        # call the init method from the parent class
-        super().__init__()
-        # call the init method from the new window class
-        self.initUI()
-
-    # create a method to initialize the new window
-    def initUI(self):
-        # set the size of the window
-        self.resize(300, 200)
-        # set the position of the window
-        self.move(600, 300)
-        # set the title of the window
-        self.setWindowTitle('Window')
-        # set the icon of the window
-        self.setWindowIcon(QIcon('web.png'))
-
-        # create a button to close the window
-        btn = QPushButton('Close', self)
-        # set the position of the button
-        btn.move(20, 20)
-        # set the size of the button
-        btn.resize(btn.sizeHint())
-        # connect the button to the close method
-        btn.clicked.connect(self.close)
-
-        # show the window
-        self.show()
-
-
-# create a class for the new window
-class DownloadConfigs(QWidget):
-
-    # create a method to initialize the new window
-    def __init__(self):
-        # call the init method from the parent class
-        super().__init__()
-        # call the init method from the new window class
-        self.initUI()
-
-    # create a method to initialize the new window
-    def initUI(self):
-        # set the size of the window
-        self.resize(300, 200)
-        # set the position of the window
-        self.move(600, 300)
-        # set the title of the window
-        self.setWindowTitle('Window')
-        # set the icon of the window
-        self.setWindowIcon(QIcon('web.png'))
-
-        # create a button to close the window
-        btn = QPushButton('Close', self)
-        # set the position of the button
-        btn.move(20, 20)
-        # set the size of the button
-        btn.resize(btn.sizeHint())
-        # connect the button to the close method
-        btn.clicked.connect(self.close)
-
-        # show the window
-        self.show()
-
-
-# create a class for the new window
-class OrionView(QWidget):
-    # create a method to initialize the new window
-    def __init__(self):
-        # call the init method from the parent class
-        super().__init__()
-        # call the init method from the new window class
-        self.initUI()
-
-    # create a method to initialize the new window
-    def initUI(self):
-        # set the size of the window
-        self.resize(300, 200)
-        # set the position of the window
-        self.move(600, 300)
-        # set the title of the window
-        self.setWindowTitle('Window')
-        # set the icon of the window
-        self.setWindowIcon(QIcon('web.png'))
-
-        # create a button to close the window
-        btn = QPushButton('Close', self)
-        # set the position of the button
-        btn.move(20, 20)
-        # set the size of the button
-        btn.resize(btn.sizeHint())
-        # connect the button to the close method
-        btn.clicked.connect(self.close)
-
-        # show the window
-        self.show()
-
-
-# create a method to run the app
-def main():
-    # create an instance of QApplication
+if __name__ == '__main__':
     app = QApplication(sys.argv)
-    # create an instance of the GUI class
     ex = App()
-    # run the app
     sys.exit(app.exec_())
 
 
-# run the app
-if __name__ == '__main__':
-    main()
+
 
