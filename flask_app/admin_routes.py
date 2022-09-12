f1d4d63b651ab69fa41a92a9dd8c74708f558825
ChangeinX <userdata0@gmail.com>
2022-09-11 22:03:56 -0500

    Save point for the night

--- flask_app/admin_routes.py
+++ flask_app/admin_routes.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, redirect, url_for, request
 from flask_paginate import Pagination, get_page_args
 
 from database import device_db_handler
@@ -6,31 +6,41 @@ from database import user_pass
 from flask_app import app
 
 
-@app.route('/admin/dashboard/<username>')
-def admin_dashboard(username):
+@app.route('/admin/dashboard/')
+def admin_dashboard():
     # build table of devices from database and paginate
     page, per_page, offset = get_page_args(page_parameter='page', per_page_parameter='per_page')
     # set default page size
-    per_page = 25
+    per_page = 15
     total = len(device_db_handler.get_device_info())
     pagination_devices = device_db_handler.get_device_info()[offset: offset + per_page]
     pagination = Pagination(page=page, per_page=per_page, total=total, css_framework='bootstrap4')
-    # only show if user is authenticated
-    if user_pass.check_admin(username):
-        return render_template('admin/templates/dashboard.html',
-                               username=username,
-                               devices=pagination_devices,
-                               page=page,
-                               per_page=per_page,
-                               pagination=pagination)
+    return render_template('admin/templates/dashboard.html',
+                           devices=pagination_devices,
+                           page=page,
+                           per_page=per_page,
+                           pagination=pagination)
+
+
+# if button id="ping" is pressed ping the device and refresh the page
+@app.route('/admin/dashboard/ping/<ip>', methods=['GET', 'POST'])
+def ping_device(ip):
+    # ping the device if submit button was pressed in the table
+    if request.method == 'POST':
+        # ping the device
+        device_db_handler.get_device_status_by_ping(ip)
+        # refresh the page
+        return redirect(url_for('admin_dashboard'))
 
 
 # dynamic route to click on a device and load its information
 @app.route('/admin/device/<device_name>', methods=['GET', 'POST'])
-def admin_device(username, name):
-    device = device_db_handler.get_device_by_name(name)
-    return render_template('admin/templates/config_device.html', username=username, device=device)
-
+def admin_device(name):
+    if request.method == 'POST':
+        device = device_db_handler.get_device_by_name(name)
+        return render_template('admin/templates/device.html', device=device)
+    else:
+        return redirect(url_for('admin_dashboard'))
 
 @app.route("/profile/<username>")
 def admin_profile(username):
